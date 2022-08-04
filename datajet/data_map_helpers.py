82559801d7dddb94f51c8986466fd34d1f8a4835
britz <brianmritz@gmail.com>
2022-08-03 21:35:36 -0400

    remove comments and print statements

--- datajet/data_map_helpers.py
+++ datajet/data_map_helpers.py
@@ -16,12 +16,7 @@ def get_dependencies_from_normalized_datamap(
     seen: set = None,
 ):
     # Track the visited and unvisited nodes using queue
-    print()
-    print("**********CALLING FUNCTION**********")
-    print(f"{key=}")
-    print(f"{seen=}")
     seen = set() if seen is None else copy.copy(seen)
-
     queue = collections.deque([key])
     seen.add(key)
     accum = []
@@ -32,25 +27,13 @@ def get_dependencies_from_normalized_datamap(
         accum_over_possible_inputs = []
         possible_inputs = [d["in"] for d in datamap[vertex]]
         for inputs in possible_inputs:
-            # breakpoint()
-            # split out the state
-            # seen_copy = seen.union(set_of_possible_inputs['in'])
-
-            # seen_copy = copy.deepcopy(seen)
             acc = []
             circular = False
-            # breakpoint()
             for input_ in inputs:
-                # breakpoint()
                 if input_ in seen:
-                    print("ERROR")
-                    print(f"{input_=}")
-                    print(f"{seen=}")
                     circular = True
                     break
-                # if k not in seen:
                 try:
-                    print(f"{input_=}")
                     new_ancestors = get_dependencies_from_normalized_datamap(
                         datamap,
                         input_,
@@ -63,12 +46,9 @@ def get_dependencies_from_normalized_datamap(
                     acc = new_ancestors
                 else:
                     acc = [a + [x for x in n if x not in a] for n in new_ancestors for a in acc]
-                print(f"{acc=}")
-                # seen.add(k)
             if circular:
                 continue
             for path in acc:
-                print(f"{path=}")
                 accum_over_possible_inputs.append(accum + list(path))
 
         if accum_over_possible_inputs:
