fc99739ac026a1e09d8676ed09b9dd6d523db82d
britz <brianmritz@gmail.com>
2022-08-03 21:32:47 -0400

    formatting

--- tests/test_datajet.py
+++ tests/test_datajet.py
@@ -6,36 +6,22 @@ from datajet import get_dependencies, is_valid_normalized_data_map
 @pytest.mark.parametrize(
     "data_map,key,expected_result",
     [
-        ({"a": {"f": lambda: 1}}, "a", [['a']]),
-        ({"a": {"in": ["b"], "f": lambda x: 1 + x}, "b": {"f": lambda: 2}}, "a",[ ['a', "b"]]),
+        ({"a": {"f": lambda: 1}}, "a", [["a"]]),
+        ({"a": {"in": ["b"], "f": lambda x: 1 + x}, "b": {"f": lambda: 2}}, "a", [["a", "b"]]),
         (
-            {
-                "a": {"in": ["b"], "f": lambda x: 1},
-                "b": {"in": ["c"], "f": lambda x: 1},
-                "c": {"f": lambda: 1},
-            },
+            {"a": {"in": ["b"], "f": lambda x: 1}, "b": {"in": ["c"], "f": lambda x: 1}, "c": {"f": lambda: 1}},
             "a",
-            [['a', "b", "c"]],
+            [["a", "b", "c"]],
         ),
         (
-            {
-                "a": {"in": ["b"], "f": lambda x: 1},
-                "b": {"in": ["c"], "f": lambda x: 1},
-                "c": {"f": lambda: 1},
-            },
+            {"a": {"in": ["b"], "f": lambda x: 1}, "b": {"in": ["c"], "f": lambda x: 1}, "c": {"f": lambda: 1}},
             "b",
-            [[
-                'b', "c",
-            ]],
+            [["b", "c"]],
         ),
         (
-            {
-                "a": {"in": ["b", "c"], "f": lambda x, y: 1},
-                "b": {"in": ["c"], "f": lambda x: 1},
-                "c": {"f": lambda: 1},
-            },
+            {"a": {"in": ["b", "c"], "f": lambda x, y: 1}, "b": {"in": ["c"], "f": lambda x: 1}, "c": {"f": lambda: 1}},
             "a",
-            [['a', 'b','c']],
+            [["a", "b", "c"]],
         ),
     ],
 )
