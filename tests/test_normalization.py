80459a1777119b48eb9879df567240e706646efb
britz <brianmritz@gmail.com>
2022-08-27 21:43:54 -0400

    update formatting

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -10,12 +10,14 @@ def assert_structure(ll):
         assert "f" in d
         assert "in" in d
 
+
 def test_norm_for_constant_list_gives_unexpected_result():
-    v = [1,2,3,4]
+    v = [1, 2, 3, 4]
     r = _norm(v)
     assert_structure(r)
     assert len(r) == 4
-    assert all([r[i]['f']() == j for i,j in zip(range(4), v)])
+    assert all([r[i]["f"]() == j for i, j in zip(range(4), v)])
+
 
 @pytest.mark.parametrize("f", [None, lambda x: [x]])
 def test_norm_for_singleton(f):
@@ -79,4 +81,3 @@ def test_norm_for_lambda_2_args_w_f_key_and_in_key(f):
     assert_structure(r)
     assert r[0]["in"] == ["this", "that"]
     assert r[0]["f"](2, 5) == 8
-
