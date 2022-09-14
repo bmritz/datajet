babdc1e59ae23f798f62f98ba508d88525ad6572
britz <brianmritz@gmail.com>
2022-09-13 22:21:57 -0400

    add the dict_resolver common resolver

--- datajet/common_resolvers.py
+++ datajet/common_resolvers.py
@@ -1,6 +1,8 @@
 """Common resolvers that can be used as shortcuts in creating a datamap."""
 
 
+from typing import Hashable
+
 from .exceptions import RuntimeResolutionException
 
 
@@ -24,3 +26,19 @@ def required_from_context():
     """
 
     return _REQUIRED_FROM_CONTEXT
+
+
+def dict_resolver(input_node: Hashable, d: dict) -> list[dict]:
+    """Returns a resolver function that looks up the resulting value from `d` corresponding with the key output from the `input_node`.
+
+    Notes:
+        The resolver will raise RuntimeResolutionException if the key is not found in the dict at "resolution time."
+    """
+
+    def _f(key):
+        try:
+            return d[key]
+        except KeyError:
+            raise RuntimeResolutionException
+
+    return [{"in": [input_node], "f": _f}]
